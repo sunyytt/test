298b0b94204256fa8af7d7a9acf9449708441d64
sunyy <407012881@qq.com>
2020-04-27 17:25:11 +0800

    记录用户登录记录

---
+++ galadriel-system/src/main/java/me/fun/system/repository/UserOnlineRepository.java
@@ -0,0 +1,8 @@
+package me.fun.system.repository;
+
+import me.fun.system.entity.domain.UserOnline;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UserOnlineRepository extends JpaRepository<UserOnline, Long>, JpaSpecificationExecutor<UserOnline> {
+}
